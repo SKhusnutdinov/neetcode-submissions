d06f3acea32f0948df3095d2538d4739e165daf9
Salavat Khusnutdinov <salavatkhusnutdinovwork@gmail.com>
2026-06-29 13:01:38 +0300

    Add: score-of-a-string - submission-3

---
+++ Data Structures & Algorithms/score-of-a-string/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        su = 0
+
+        for i in range(1, len(s)):
+            su += (abs(ord(s[i-1]) - ord(s[i])))
+        
+        return su
