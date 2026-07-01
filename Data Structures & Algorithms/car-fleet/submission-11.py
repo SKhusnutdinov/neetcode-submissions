3c6c972c9e41306c275302e0b31b0c267a51e111
Salavat Khusnutdinov <salavatkhusnutdinovwork@gmail.com>
2026-07-01 15:41:32 +0300

    Add: car-fleet - submission-11

---
+++ Data Structures & Algorithms/car-fleet/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [(p, s) for p, s in zip(position, speed)]
+
+        pairs.sort(reverse=True)
+        print(pairs)
+        st = []
+
+        for pair in pairs:
+            p, s = pair
+            t = (target - p) / s
+
+            if not st:
+                st.append(t)
+            if st and not st[-1] >= t:
+                st.append(t)
+
+        return len(st)
