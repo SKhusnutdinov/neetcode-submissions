6fe734f6da6fa7aca1f297ba6fab0a8674387877
Salavat Khusnutdinov <salavatkhusnutdinovwork@gmail.com>
2026-07-04 13:03:49 +0300

    Add: reverse-linked-list-ii - submission-6

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-6.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
+        l1 = dummy.next
+
+        for _ in range(1, left):
+            prev = prev.next
+            l1 = l1.next
+
+
+        revPrev = None
+
+        for _ in range(right - left + 1):
+            nxt = l1.next
+            l1.next = revPrev
+            revPrev = l1
+            l1 = nxt
+
+        prev.next.next = l1
+        prev.next = revPrev
+
+        
+        return dummy.next
