ec746b029062e8be76b5256f27ccf2e32c97345b
Salavat Khusnutdinov <salavatkhusnutdinovwork@gmail.com>
2026-07-09 19:29:23 +0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hm = defaultdict(list)
+
+        for word in strs:
+            alpha = [0] * 26
+
+            for ch in word:
+                alpha[ord(ch) - ord('a')] += 1
+            
+            hm[tuple(alpha)].append(word)
+
+        
+        res = []
+        
+        for val in hm.values():
+            res.append(val)
+        
+        return res
