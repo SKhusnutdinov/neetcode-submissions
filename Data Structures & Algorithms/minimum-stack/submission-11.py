3dfcd180f008800dab22934cbf4910ad85f93b9b
Salavat Khusnutdinov <salavatkhusnutdinovwork@gmail.com>
2026-07-09 20:01:52 +0300

    Add: minimum-stack - submission-11

---
+++ Data Structures & Algorithms/minimum-stack/submission-11.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.st = []
+        self.stMin = []
+
+    def push(self, val: int) -> None:
+        self.st.append(val)
+        val = min(val, self.stMin[-1] if self.stMin else val)
+        self.stMin.append(val)
+        return
+            
+
+    def pop(self) -> None:
+        self.st.pop()
+        self.stMin.pop()
+        return
+
+    def top(self) -> int:
+        return self.st[-1]
+
+    def getMin(self) -> int:
+        return self.stMin[-1]
