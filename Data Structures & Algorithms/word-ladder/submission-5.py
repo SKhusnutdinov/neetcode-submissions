07c8df59afdb1984b549483e75f0838e6fde8890
Salavat Khusnutdinov <salavatkhusnutdinovwork@gmail.com>
2026-08-01 01:41:53 +0300

    Add: word-ladder - submission-5

---
+++ Data Structures & Algorithms/word-ladder/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        words = set(wordList)
+
+        q = deque()
+        q.append(beginWord)
+
+        visit = set()
+        res = 1
+
+        while q:
+            for _ in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                
+                visit.add(word)
+                wordForm = list(word)
+                
+                for i in range(len(word)):
+                    changeWordForm = wordForm.copy()
+                    for ch in range(ord('a'), ord('z') + 1):
+                        changeWordForm[i] = chr(ch)
+                        changedWord = "".join(changeWordForm)
+                        if (changedWord in words and
+                            changedWord not in visit
+                            ):
+                            q.append(changedWord)
+
+            res += 1
+
+        return 0
+
